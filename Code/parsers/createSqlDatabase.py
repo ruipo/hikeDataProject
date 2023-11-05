7e910321170287fc0f99ef62e3dd6566dae2ee10
Rui <47530765+ruipo@users.noreply.github.com>
2023-11-05 16:05:37 -0500

    wrote python code to read in workout route data and put into sql database

---
+++ Code/parsers/createSqlDatabase.py
@@ -0,0 +1,64 @@
+import psycopg2 as psql
+import pandas as pd
+import os
+
+
+def create_db_connection(host_name, user_name, user_pwd, db_name, db_port):
+    connection = None
+    try:
+        connection = psql.connect(database=db_name,
+                                  host=host_name,
+                                  user=user_name,
+                                  password=user_pwd,
+                                  port=db_port)
+        print('Successfully connected to database!')
+    except Exception as err:
+        print("Error connecting to database: " + str(err) + "...")
+    return connection
+
+
+def execute_query(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+        # print('query executed successfully!')
+
+    except Exception as err:
+        print("Error executing query: " + str(err) + "...")
+
+
+# Connect to healthdata database
+connection = create_db_connection('localhost', 'Rui', 'rui123chen45',
+                                  'healthdata', 5432)
+# Create workoutRoutes table
+query = """
+CREATE TABLE workout_routes (
+workout_id INT NOT NULL,
+time TIMESTAMP NOT NULL,
+latitude DECIMAL(10,6) NOT NULL,
+longitude DECIMAL(10,6) NOT NULL,
+altitude DECIMAL(10,6) NOT NULL );
+"""
+execute_query(connection, query)
+connection.commit()
+
+# Populate workoutRoutes table with csv data
+workOutRoutesPath = '/Users/Rui/Documents/hikeData/Data/csv/workoutRoutes/'
+workOutRoutesDir = os.listdir(workOutRoutesPath)
+workOutFiles = [f for f in workOutRoutesDir if os.path.isfile(os.path.join(workOutRoutesPath, f))]
+workOutID = 1
+
+for f in workOutFiles:
+    print('Reading in data files ' + str(workOutID) + '/' + str(len(workOutFiles)) + '...')
+    data = pd.read_csv(os.path.join(workOutRoutesPath, f))
+    for tInd in range(0, len(data) - 1):
+        pop_workout_routes = "INSERT INTO workout_routes VALUES " + \
+                             "(" + str(workOutID) + ", '" + str(data.time[tInd]) + "', '" \
+                             + str(data.latitude[tInd]) + "', '" + str(data.longitude[tInd]) \
+                             + "', '" + str(data.altitude[tInd]) + "');"
+        execute_query(connection, pop_workout_routes)
+    workOutID += 1
+
+# TO DO:
+# Make tables for heart rate, walking/running speed.
